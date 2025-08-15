5b20cd63ca28dbc6393e1d1c3702d084379526f4
Nguyen Chanh Dang <chanhdang@penphy.edu.vn>
2025-08-15 17:00:00 +0700

    feat(frmBan.cs): implement CRUD operations for Ban entity with data binding and Excel import/export functionality feat(frmBan.cs): add help documentation link and improve user experience with button state management fix(frmBan.cs): handle empty input and selection errors with user-friendly messages

--- QuanLyQuanCaPhe/Forms/frmBan.cs
+++ QuanLyQuanCaPhe/Forms/frmBan.cs
@@ -14,38 +14,117 @@ namespace QuanLyQuanCaPhe.Forms
 {
     public partial class frmBan : Form
     {
-       
+        QLQCPContext context = new QLQCPContext(); // Khởi tạo biến ngữ cảnh CSDL 
+        int id;
         public frmBan()
         {
             InitializeComponent();
-
+            helpProvider1.SetHelpString(this, "https://nguyenhauhuu.github.io/demo/");
+            this.HelpRequested += (s, e) => {
+                System.Diagnostics.Process.Start(new System.Diagnostics.ProcessStartInfo(helpProvider1.GetHelpString(this)!) { UseShellExecute = true });
+                e.Handled = true;
+            };
 
         }
+        private void BatTatChucNang(bool giaTri)
+        {
+            btnLuu.Enabled = giaTri;
+            btnHuyBo.Enabled = giaTri;
+            txtTenBan.Enabled = giaTri;
 
+            btnThem.Enabled = !giaTri;
+            btnSua.Enabled = !giaTri;
+            btnXoa.Enabled = !giaTri;
+        }
 
         private void frmBan_Load(object sender, EventArgs e)
         {
-           
+            BatTatChucNang(false);
+            dataGridView.AutoGenerateColumns = false;
+
+            var ban = context.Ban.ToList();
+            dataGridView.DataSource = ban;
+
+            txtTenBan.DataBindings.Clear();
+            txtTenBan.DataBindings.Add("Text", ban, "TenBan", false, DataSourceUpdateMode.Never);
+
+            txtID.DataBindings.Clear();
+            txtID.DataBindings.Add("Text", ban, "ID", false, DataSourceUpdateMode.Never);
         }
 
         private void btnThem_Click(object sender, EventArgs e)
         {
-           
+            id = 0;
+            BatTatChucNang(true);
+            txtTenBan.Text = "";
+            txtID.Text = "";
+            txtTenBan.Focus();
         }
 
         private void btnSua_Click(object sender, EventArgs e)
         {
-           
+            if (dataGridView.CurrentRow != null)
+            {
+                BatTatChucNang(true);
+                id = Convert.ToInt32(dataGridView.CurrentRow?.Cells[0].Value?.ToString());
+                txtTenBan.Focus();
+            }
+            else
+            {
+                MessageBox.Show("Vui lòng chọn một dòng để cập nhật.", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
         }
 
         private void btnLuu_Click(object sender, EventArgs e)
         {
-            
+            if (string.IsNullOrWhiteSpace(txtTenBan.Text))
+                MessageBox.Show("Vui lòng nhập tên ban?", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            else
+            {
+                if (id == 0)
+                {
+                    Ban ban = new Ban();
+                    ban.TenBan = txtTenBan.Text;
+                    context.Ban.Add(ban);
+
+                    context.SaveChanges();
+                }
+                else
+                {
+                    Ban ban = context.Ban.Find(id)!;
+                    if (ban != null)
+                    {
+                        ban.TenBan = txtTenBan.Text;
+                        context.Ban.Update(ban);
+
+                        context.SaveChanges();
+                    }
+                }
+
+                frmBan_Load(sender, e);
+            }
         }
 
         private void btnXoa_Click(object sender, EventArgs e)
         {
-           
+            if (dataGridView.CurrentRow != null)
+            {
+                if (MessageBox.Show("Xác nhận xóa " + txtTenBan.Text + "?", "Xóa", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+                {
+                    id = Convert.ToInt32(dataGridView.CurrentRow?.Cells[0].Value?.ToString());
+                    Ban ban = context.Ban.Find(id)!;
+                    if (ban != null)
+                    {
+                        context.Ban.Remove(ban);
+                        context.SaveChanges();
+                    }
+                    frmBan_Load(sender, e);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Vui lòng chọn một dòng để xóa.", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
         }
 
         private void btnHuyBo_Click(object sender, EventArgs e)
@@ -55,25 +134,122 @@ namespace QuanLyQuanCaPhe.Forms
 
         private void btnNhap_Click(object sender, EventArgs e)
         {
-            
-            
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Title = "Nhập dữ liệu từ tập tin Excel";
+            openFileDialog.Filter = "Tập tin Excel|*.xls;*.xlsx";
+            openFileDialog.Multiselect = false;
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    using (XLWorkbook workbook = new XLWorkbook(openFileDialog.FileName))
+                    {
+                        IXLWorksheet worksheet = workbook.Worksheet(1);
+                        bool firstRow = true;
+                        string readRange = "";
+                        DataTable table = new DataTable();
+
+                        // Đọc Sheet 1 và lưu dữ liệu vào một bảng tạm 
+                        foreach (IXLRow row in worksheet.RowsUsed())
+                        {
+                            // Đọc dòng tiêu đề (dòng đầu tiên) 
+                            if (firstRow)
+                            {
+                                readRange = string.Format("{0}:{1}", 1, row.LastCellUsed().Address.ColumnNumber);
+                                foreach (IXLCell cell in row.Cells(readRange))
+                                    table.Columns.Add(cell.Value.ToString());
+                                firstRow = false;
+                            }
+                            else // Đọc các dòng nội dung (các dòng tiếp theo) 
+                            {
+                                table.Rows.Add();
+                                int cellIndex = 0;
+                                foreach (IXLCell cell in row.Cells(readRange))
+                                {
+                                    table.Rows[table.Rows.Count - 1][cellIndex] = cell.Value.ToString();
+                                    cellIndex++;
+                                }
+                            }
+                        }
+
+                        // Đọc dữ liệu từ bảng tạm và lưu vào CSDL 
+                        if (table.Rows.Count > 0)
+                        {
+                            foreach (DataRow r in table.Rows)
+                            {
+                                Ban ban = new Ban();
+                                ban.TenBan = r["TenBan"].ToString() ?? "N/A";
+                                context.Ban.Add(ban);
+                            }
+                            context.SaveChanges();
+
+                            MessageBox.Show("Đã nhập thành công " + table.Rows.Count + " dòng.", "Thành công", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                            frmBan_Load(sender, e);
+                        }
+                        else
+                            MessageBox.Show("Tập tin Excel rỗng.", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                }
+            }
         }
 
         private void btnXuat_Click(object sender, EventArgs e)
         {
-            
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Title = "Xuất dữ liệu ra tập tin Excel";
+            saveFileDialog.Filter = "Tập tin Excel|*.xls;*.xlsx";
+            saveFileDialog.FileName = "Ban_" + DateTime.Now.ToShortDateString().Replace("/", "_") + ".xlsx";
+
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    // Đọc dữ liệu từ CSDL và lưu vào một bảng tạm 
+                    DataTable table = new DataTable();
+
+                    table.Columns.AddRange(new DataColumn[] {
+                        new DataColumn("ID", typeof(int)),
+                        new DataColumn("TenBan", typeof(string))
+
+                       });
+
+                    var ban = context.Ban.ToList();
+                    if (ban != null)
+                    {
+                        foreach (var p in ban)
+                            table.Rows.Add(p.ID, p.TenBan);
+                    }
+
+                    // Gán bảng tạm vào Sheet 1 của tập tin Excel 
+                    using (XLWorkbook wb = new XLWorkbook())
+                    {
+                        var sheet = wb.Worksheets.Add(table, "Ban");
+                        sheet.Columns().AdjustToContents();
+                        wb.SaveAs(saveFileDialog.FileName);
+
+                        MessageBox.Show("Đã xuất dữ liệu ra tập tin Excel thành công.", "Thành công", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                }
+            }
         }
 
         private void btnTimKiem_Click(object sender, EventArgs e)
         {
 
-           
+            var ban = context.Ban
+            .Where(r => r.TenBan.Contains(txtTuKhoa.Text))
+            .ToList();
+            dataGridView.DataSource = ban;
 
-        }
-
-        private void btnThoat_Click(object sender, EventArgs e)
-        {
-            this.Close();
         }
     }
 }
